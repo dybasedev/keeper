bb7fbfae63038888d3c12c0cfebbe96de5abc1da
chongyi <xpz3847878@163.com>
2017-10-16 14:28:27 +0800

    初始化项目

---
+++ src/Http/Websocket/Server.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Server.php
+ *
+ * @copyright Chongyi <xpz3847878@163.com>
+ * @link      https://insp.top
+ */
+
+namespace Dybasedev\Keeper\Http\Websocket;
+
+use Dybasedev\Keeper\Http\Server as HttpServer;
+
+class Server extends HttpServer
+{
+
+}
---
+++ src/Server/Server.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Server.php
+ *
+ * @copyright Chongyi <xpz3847878@163.com>
+ * @link      https://insp.top
+ */
+
+namespace Dybasedev\Keeper\Server;
+
+
+abstract class Server
+{
+
+}
