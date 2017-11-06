bfa2bde9b0f356716296e83136ce22e1b86dc868
chongyi <xpz3847878@163.com>
2017-11-06 16:58:12 +0800

    path register

--- src/Http/ContextContainer.php
+++ src/Http/ContextContainer.php
@@ -12,5 +12,37 @@ use Illuminate\Container\Container;
 
 class ContextContainer extends Container
 {
+    /**
+     * @var string
+     */
+    public $basePath;
 
+    /**
+     * @var
+     */
+    public $applicationPath;
+
+    /**
+     * ContextContainer constructor.
+     *
+     * @param $basePath
+     */
+    public function __construct(string $basePath)
+    {
+        $this->setBasePath($basePath);
+    }
+
+    public function setBasePath(string $basePath)
+    {
+        $this['path.base'] = $this->basePath = realpath($basePath);
+
+        return $this;
+    }
+
+    public function setApplicationPath(string $appPath)
+    {
+        $this['path.app'] = $this->applicationPath = realpath($appPath);
+
+        return $this;
+    }
 }
