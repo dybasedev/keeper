335abe8710beb98af8336205d4f479024b68828f
chongyi <xpz3847878@163.com>
2017-11-13 15:18:24 +0800

    illuminate route provider

---
+++ src/Http/Providers/RoutingProvider.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * RoutingProvider.php
+ *
+ * @copyright Chongyi <xpz3847878@163.com>
+ * @link      https://insp.top
+ */
+
+namespace Dybasedev\Keeper\Http\Providers;
+
+
+use Dybasedev\Keeper\Http\ContextContainer;
+use Dybasedev\Keeper\Http\ModuleProvider;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\ResponseFactory;
+use Illuminate\Routing\UrlGenerator;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Illuminate\Contracts\View\Factory as ViewFactoryContract;
+use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
+
+class RoutingProvider extends ModuleProvider
+{
+    public function register()
+    {
+        $this->registerUrlGenerator();
+        $this->registerRedirector();
+        $this->registerPsrRequest();
+        $this->registerResponseFactory();
+    }
+
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the URL generator service.
+     *
+     * @return void
+     */
+    protected function registerUrlGenerator()
+    {
+        $this->container->singleton('url', function (ContextContainer $container) {
+            $routes = $container['router']->getRoutes();
+
+            // The URL generator needs the route collection that exists on the router.
+            // Keep in mind this is an object, so we're passing by references here
+            // and all the registered routes will be available to the generator.
+            $container->instance('routes', $routes);
+
+            $url = new UrlGenerator(
+                $routes, $container->rebinding('request', $this->requestRebinder())
+            );
+
+            $url->setSessionResolver(function () {
+                return null; // Keeper 不暂不支持 Illuminate 的 Session 组件
+            });
+
+            // If the route collection is "rebound", for example, when the routes stay
+            // cached for the application, we will need to rebind the routes on the
+            // URL generator instance so it has the latest version of the routes.
+            $container->rebinding('routes', function ($container, $routes) {
+                $container['url']->setRoutes($routes);
+            });
+
+            return $url;
+        });
+    }
+
+    /**
+     * Get the URL generator request rebinder.
+     *
+     * @return \Closure
+     */
+    protected function requestRebinder()
+    {
+        return function ($app, $request) {
+            $app['url']->setRequest($request);
+        };
+    }
+
+    /**
+     * Register the Redirector service.
+     *
+     * @return void
+     */
+    protected function registerRedirector()
+    {
+        $this->container->singleton('redirect', function (ContextContainer $container) {
+            $redirector = new Redirector($container['url']);
+
+            // If the session is set on the application instance, we'll inject it into
+            // the redirector instance. This allows the redirect responses to allow
+            // for the quite convenient "with" methods that flash to the session.
+            if (isset($container['session.store'])) {
+                $redirector->setSession($container['session.store']);
+            }
+
+            return $redirector;
+        });
+    }
+
+    /**
+     * Register a binding for the PSR-7 request implementation.
+     *
+     * @return void
+     */
+    protected function registerPsrRequest()
+    {
+        $this->container->bind(ServerRequestInterface::class, function ($app) {
+            return (new DiactorosFactory)->createRequest($app->make('request'));
+        });
+    }
+
+
+    /**
+     * Register the response factory implementation.
+     *
+     * @return void
+     */
+    protected function registerResponseFactory()
+    {
+        $this->container->singleton(ResponseFactoryContract::class, function ($app) {
+            return new ResponseFactory($app[ViewFactoryContract::class], $app['redirect']);
+        });
+    }
+
+
+}
